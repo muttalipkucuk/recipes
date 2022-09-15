cabfcdfc5224b2a6ad47cafbda4f8dee8d9ccb28
Muttalip Küçük <muttalip.kucuk@inergy.nl>
2022-09-15 21:26:13 +0200

    Second commit

---
+++ src/test/java/com/kucuktech/lab/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kucuktech.lab.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
