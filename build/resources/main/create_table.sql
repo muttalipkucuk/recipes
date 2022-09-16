974ddf2233b52b35f5e49ec85b3e140924775dd2
Muttalip Küçük <muttalip.kucuk@inergy.nl>
2022-09-16 10:55:42 +0200

    Removed build-folder

--- build/resources/main/create_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS dish;
-
--- Create new table named `dish`
-CREATE TABLE dish(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    vegetarian BOOLEAN,
-    servings INT,
-    ingredients TEXT ARRAY,
-    instructions TEXT);
